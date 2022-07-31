22b113233295416836f60df2d7d078549bef7248
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-07-31 23:22:29 +0000

    add compiler service into core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,12 +1,13 @@
 import { HttpModule } from '@nestjs/axios';
 import { Global, Module } from '@nestjs/common';
+import { CompilerService } from './compiler/compiler.service';
 import { JudgeService } from './compiler/judge/judge.service';
 import { DatabaseModule } from './database/database.module';
 
 @Global()
 @Module({
 	imports: [DatabaseModule, HttpModule],
-	providers: [JudgeService],
-	exports: [JudgeService],
+	providers: [JudgeService, CompilerService],
+	exports: [JudgeService, CompilerService],
 })
 export class CoreModule {}
