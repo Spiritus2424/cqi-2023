888169f14dd34c10048a3f0d1c54e3642dce003b
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-07-31 23:21:57 +0000

    add compiler service

---
+++ src/core/compiler/compiler.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { Compiler } from '@prisma/client';
+import { PrismaService } from '../database/prisma/prisma.service';
+
+@Injectable()
+export class CompilerService {
+	constructor(private readonly _prismaService: PrismaService) {}
+
+	async findMany(name?: string): Promise<Compiler[]> {
+		return this._prismaService.compiler.findMany({
+			where: {
+				name: {
+					contains: name,
+					mode: 'insensitive',
+				},
+			},
+		});
+	}
+
+	async findOne(id: number): Promise<Compiler> {
+		return this._prismaService.compiler.findUnique({
+			where: {
+				id: id,
+			},
+		});
+	}
+}
