20b61e400fdb35319142ba12c2048a686e2dfd27
Ahmad Faour <ahmad.faour@polymtl.ca>
2022-08-01 06:32:16 +0000

    add findOne method in code-template service

--- src/core/code-template/code-template.service.ts
+++ src/core/code-template/code-template.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CodeTemplate } from '@prisma/client';
+import { PrismaService } from '../database/prisma/prisma.service';
 
 @Injectable()
-export class CodeTemplateService {}
+export class CodeTemplateService {
+	constructor(private readonly _prismaService: PrismaService) {}
+
+	async findOne(compilerId: number, problemId: number): Promise<CodeTemplate> {
+		return this._prismaService.codeTemplate.findUnique({
+			where: {
+				problemId_compilerId: { compilerId: compilerId, problemId: problemId },
+			},
+		});
+	}
+}
