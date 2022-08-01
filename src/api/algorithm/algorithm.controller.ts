293450258580c8ef606656e82d1adfd0fd502b18
Ahmad Faour <ahmad.faour@polymtl.ca>
2022-08-01 06:33:43 +0000

    implement submitCode method

--- src/api/algorithm/algorithm.controller.ts
+++ src/api/algorithm/algorithm.controller.ts
@@ -1,34 +1,25 @@
-import {
-	Body,
-	Controller,
-	Post,
-	UploadedFile,
-	UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CodeSolutionService } from 'src/core/code-solution/code-solution.service';
 import { JudgeService } from '../../core/judge/judge.service';
 import { SubmitCodeDto } from './dto/submit-code.dto';
 
 @Controller('algorithm')
 export class AlgorithmController {
-	constructor(private readonly _judgeService: JudgeService) {}
+	constructor(
+		private readonly _judgeService: JudgeService,
+		private readonly _codeSolutionService: CodeSolutionService,
+	) {}
 
 	@Post('submit-code')
-	@UseInterceptors(FileInterceptor('file'))
-	async submitCode(
-		@UploadedFile() file: Express.Multer.File,
-		@Body() body: SubmitCodeDto,
-	): Promise<string> {
-		console.log(file);
-		// console.log(body.code);
-		// console.log(body.compilerId);
-		console.log(typeof body.compilerId === 'number');
-		// console.log(body instanceof SubmitCodeDto);
-		// return await this._judgeService.submitCode({
-		// 	compilerId: body.compilerId,
-		// 	sourceCode: file.buffer.toString(),
-		// });
-
-		return '';
+	async submitCode(@Body() body: SubmitCodeDto): Promise<string> {
+		const codeSolution = await this._codeSolutionService.findOne(
+			body.compilerId,
+			body.problemId,
+		);
+		console.log(codeSolution.solution);
+		return this._judgeService.submitCode({
+			compilerId: body.compilerId,
+			sourceCode: codeSolution.solution,
+		});
 	}
 }
