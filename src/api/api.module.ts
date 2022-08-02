802363fae908c68e63d7f3f3b7fc32233fbbde5e
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-02 01:16:50 +0000

    remove default app controler and service

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AlgorithmController } from './algorithm/algorithm.controller';
 import { CompilerController } from './compiler/compiler.controller';
+import { OpenApiController } from './open-api/open-api.controller';
 
 @Module({
-	controllers: [CompilerController, AlgorithmController],
+	controllers: [CompilerController, AlgorithmController, OpenApiController],
 })
 export class ApiModule {}
