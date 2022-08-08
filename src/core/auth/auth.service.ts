7ede720d6ec50c909f8bcad3f59aa190899ab629
Ahmad Faour <ahmad.faour@polymtl.ca>
2022-08-08 21:40:37 +0000

    add auth service with bcrypt encryption

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-	constructor(private readonly _userService: UserService) {}
+	constructor(
+		private readonly _userService: UserService,
+		private readonly _jwtService: JwtService,
+	) {}
 
-	async validateUser(email: string, password: string): Promise<any> {
-		const user = await this._userService.findOne(email);
-		if (user && user.password === password) {
-			const { password, ...rest } = user;
-			return rest;
+	async validateUser(email: string, password: string): Promise<User> {
+		let user = await this._userService.findOne(email);
+
+		if (user && (await bcrypt.compare(password, user.password))) {
+			delete user.password;
+		} else {
+			user = null;
 		}
-		return null;
+		return user;
+	}
+
+	async login(user: User): Promise<string> {
+		return this._jwtService.sign({
+			firstName: user.firstName,
+			lastName: user.lastName,
+			sub: user.id,
+		});
+	}
+
+	async signUp(user: User): Promise<User> {
+		const hashPassword = await bcrypt.hash(
+			user.password,
+			await bcrypt.genSalt(),
+		);
+		user.password = hashPassword;
+		return await this._userService.create(user);
 	}
 }
