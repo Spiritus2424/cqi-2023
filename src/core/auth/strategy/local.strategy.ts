ee46303955e38b40a6e6b6253dfd7425b23f9126
Ahmad Faour <ahmad.faour@polymtl.ca>
2022-08-08 21:37:50 +0000

    add local strategy validation

--- src/core/auth/strategy/local.strategy.ts
+++ src/core/auth/strategy/local.strategy.ts
@@ -1,15 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
 	constructor(private readonly _authService: AuthService) {
-		super();
+		super({
+			usernameField: 'email',
+			passwordField: 'password',
+		});
 	}
 
-	async validate(email: string, passport: string): Promise<any> {
+	async validate(email: string, passport: string): Promise<User> {
 		const user = await this._authService.validateUser(email, passport);
 
 		if (!user) {
