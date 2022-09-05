ebde578834295a75505629c63287d62e2ee30996
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-09-05 22:27:23 +0000

    add simple problem table

---
+++ prisma/migrations/20220905222433_simeple_problem/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "SimpleProblem" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+
+    CONSTRAINT "SimpleProblem_pkey" PRIMARY KEY ("id")
+);
